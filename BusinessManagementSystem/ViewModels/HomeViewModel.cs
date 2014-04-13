5407d52611155f9763e5509006be9f0db2157dbd
Rakesh Nayak <rakeshn@mindfiresolutions.com>
2014-04-13 10:36:34 +0530

    Commit entity changes

---
+++ BusinessManagementSystem/ViewModels/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using BusinessManagementSystem.HelperClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessManagementSystem.ViewModels
+{
+    public class HomeViewModel : ObservableObject, IPageViewModel
+    {
+        #region Properties
+
+        public string Name
+        {
+            get
+            {
+                return "Home";
+            }
+        }
+
+        #endregion
+    }
+}
